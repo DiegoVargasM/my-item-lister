7bb6c56f57b92ed4fc41b2e09571aa00dc419c27
diegoxwo <diegovargasmiranda@gmail.com>
2022-09-20 20:15:02 -0500

    Add filter function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let form = document.getElementById("addForm");
 let itemList = document.getElementById("items");
+let filter = document.getElementById("filter");
 
 //Form submit event
 form.addEventListener("submit", addItem);
 
 //Delete item event
-itemList.addEventListener("click",removeItem);
+itemList.addEventListener("click", removeItem);
+
+//Filter event
+filter.addEventListener("keyup", filterItems);
 
 //Add item function
 function addItem(e) {
@@ -49,4 +53,23 @@ function removeItem(e) {
             itemList.removeChild(li);
         }
     }
+}
+
+//filter items function
+function filterItems(e) {
+    //convert text to lowercase
+    let text = e.target.value.toLowerCase();
+    
+    //Get all the elements in the list
+    let items = itemList.getElementsByTagName("li");
+
+    //convert to an array
+    Array.from(items).forEach(function (item) {
+        let itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    })
 }
